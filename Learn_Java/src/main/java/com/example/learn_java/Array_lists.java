634ba4fdcd1d8a4abd270b08134126d2e04f5667
notshibe <64983943+notshibe@users.noreply.github.com>
2020-05-08 17:04:46 +0100

    completed array lists

--- Learn_Java/src/main/java/com/example/learn_java/Array_lists.java
+++ Learn_Java/src/main/java/com/example/learn_java/Array_lists.java
@@ -1,4 +1,29 @@
 package com.example.learn_java;
 
+import java.util.ArrayList;
+
 public class Array_lists {
+    public static void main(String[] args) {
+        ArrayList names = new ArrayList();
+
+        names.add("cat food");
+        names.add("fookin prawns");
+        names.add("matthew");
+
+        System.out.println(names.get(1));
+
+
+
+        names.remove("cat food");
+
+        System.out.println(names.get(1));
+
+        //this won't work because array lists different to array
+        //System.out.println(names[1]);
+
+        names.remove(0);
+
+        System.out.println(names.get(0));
+
+    }
 }
