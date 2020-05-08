e1907e3cd9322d01473e9845e933fe5405e63244
notshibe <64983943+notshibe@users.noreply.github.com>
2020-05-08 16:56:42 +0100

    completed arrays started array lists

--- Learn_Java/src/main/java/com/example/learn_java/Arrays.java
+++ Learn_Java/src/main/java/com/example/learn_java/Arrays.java
@@ -4,13 +4,43 @@ public class Arrays {
     public static void main(String[] args) {
 
         //Tom (baseball batting average)
-        int game1 = 250;
-        int game2 = 400;
-        int game3 = 600;
+        //int game1 = 250;
+        //int game2 = 400;
+        //int game3 = 600;
 
-        int[] myArray = new int[10];
+        //step creates empty array of type given
+        int[] tomsAverages = new int[3];
         double[] dblArray;
         String[] names;
+        names = new String[5];
 
+        names[0] = "John";
+        names[1] = "Jacob";
+        names[2] = "Jingle";
+        names[3] = "Heimer";
+        names[4] = "Kevin";
+
+        tomsAverages[0] = 250;
+        tomsAverages[1] = 400;
+        tomsAverages[2] = 600;
+
+        System.out.println("Val 1: " + tomsAverages[0]);
+        System.out.println("Val 2: " + tomsAverages[1]);
+        System.out.println("Val 3: " + tomsAverages[2]);
+
+        tomsAverages[0] = 100;
+        System.out.println(tomsAverages[0]);
+
+        String[] top6Cars = {
+         "GTR","Aston","Lambo","GTI","Chevy","Tesla"
+        };
+
+
+        //brackets supported for maths
+        System.out.println(top6Cars[3]);
+        int lastno = top6Cars.length;
+
+        String lastcar = top6Cars[lastno-1];
+        System.out.println(lastcar);
     }
 }
