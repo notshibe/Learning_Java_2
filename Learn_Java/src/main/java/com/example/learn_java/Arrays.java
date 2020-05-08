275fb8204b9fd0e728eee65a92eef15e9151a5a6
notshibe <64983943+notshibe@users.noreply.github.com>
2020-05-08 16:13:51 +0100

    started arrays

---
+++ Learn_Java/src/main/java/com/example/learn_java/Arrays.java
@@ -0,0 +1,16 @@
+package com.example.learn_java;
+
+public class Arrays {
+    public static void main(String[] args) {
+
+        //Tom (baseball batting average)
+        int game1 = 250;
+        int game2 = 400;
+        int game3 = 600;
+
+        int[] myArray = new int[10];
+        double[] dblArray;
+        String[] names;
+
+    }
+}
