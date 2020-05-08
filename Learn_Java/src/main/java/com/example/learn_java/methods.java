cd5af191c1c1ba7da9b67fd90d9595aa578e26bb
notshibe <64983943+notshibe@users.noreply.github.com>
2020-05-08 11:40:03 +0100

    exploring different types of functions

--- Learn_Java/src/main/java/com/example/learn_java/methods.java
+++ Learn_Java/src/main/java/com/example/learn_java/methods.java
@@ -6,9 +6,14 @@ public class methods {
         //calling a function
         printName();
         printName();
+
         int len = 10;
         int wid = 5;
         printArea51(len,wid);
+
+        //stores result of running function as variable called area
+        int area = getArea(5,6);
+        System.out.println(area);
     }
 
     //void means return nothing
@@ -18,9 +23,22 @@ public class methods {
     }
 
     //inside brackets are arguments/variables to feed function
-    //public means globally accessible
+    //public means accessible by all classes and other packages
+    //protected means accessible only by classes in same package
     //maths operation needs to be contained in brackets, else will just append later no's
-    private static void printArea51(int length, int width){
+    //void means we can only use the output once, not generate variables for future use
+    public static void printArea51(int length, int width){
         System.out.println("Area: " + (length * width + 1));
+    }
+
+    //int means the output is an integer that we can use as a variable for future calculations
+    //private means only operations inside this class can access this variable
+    private static int getArea(int length, int width) {
+
+        int area = length * width;
+        //return is end of code in function
+        return area;
+
+        //return 0; statement unreachable because first return is end of code in function
     }
 }
