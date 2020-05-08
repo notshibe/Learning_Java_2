2ea105d9747271385def5ef873ad9542ad8feb85
notshibe <64983943+notshibe@users.noreply.github.com>
2020-05-08 10:39:06 +0100

    Tidied up numbers and strings Fixed strings public class

--- Learn_Java/src/main/java/com/example/learn_java/Numbers.java
+++ Learn_Java/src/main/java/com/example/learn_java/Numbers.java
@@ -1,4 +1,21 @@
 package com.example.learn_java;
 
 public class Numbers {
+    public static void main(String[] args) {
+
+        int age = 25; //int doesn't support decimals, integers only
+        int myFavoriteSports = -50;
+
+        int sum = 5 + 5;
+
+        int num1 = 10;
+        int num2 = 5000;
+
+
+
+        System.out.println(sum);
+        System.out.println(num1 + num2);
+
+
+    }
 }
--- Learn_Java/src/main/java/com/example/learn_java/Strings.java
+++ Learn_Java/src/main/java/com/example/learn_java/Strings.java
@@ -1,6 +1,6 @@
 package com.example.learn_java;
 
-public class Scratch_pad_3 {
+public class Strings {
     public static void main(String[] args) { //main is entry point
         String vehicle = "Truck"; //string: text, not for things to do maths with
         //int myBankAccount = 500; //numbers for maths
