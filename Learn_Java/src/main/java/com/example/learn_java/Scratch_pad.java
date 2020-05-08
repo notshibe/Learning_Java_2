07497a3ba9b7241b8ced1b382bdf36acb9a28efc
notshibe <64983943+notshibe@users.noreply.github.com>
2020-05-08 09:29:31 +0100

    started new scratchpad
    played with strings

--- Learn_Java/src/main/java/com/example/learn_java/Scratch_pad.java
+++ Learn_Java/src/main/java/com/example/learn_java/Scratch_pad.java
@@ -3,5 +3,6 @@ package com.example.learn_java;
 public class Scratch_pad {
     public static void main(String[] args) {
         System.out.println("Hello Maximus");
+
     }
 }
---
+++ Learn_Java/src/main/java/com/example/learn_java/Scratch_pad_2.java
@@ -0,0 +1,12 @@
+package com.example.learn_java;
+
+public class Scratch_pad_2 {
+    public static void main(String[] args) {
+
+        String name = "John";
+        int homeRuns = 55;
+        float bankBalance = 100.44f;
+
+        System.out.println(name + " has " + homeRuns + " home runs!");
+    }
+}
